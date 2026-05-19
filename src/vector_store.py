cddd1103bdbc7a31211c2ef32a9c2d4e7597db68
Ravindu Saputhanthri <104779889+ravinduWP@users.noreply.github.com>
2026-05-19 13:16:01 +0530

    Add client settings to build_vectorstore function
    Update build_vectorstore to include client settings for Chroma.

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,11 +1,17 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
+import chromadb
 def load_embedding():
     return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 def build_vectorstore(chunks, embeddings):
-    vectorstore = Chroma.from_texts(chunks, embeddings)
+    vectorstore = Chroma.from_texts(
+    chunks, 
+    embeddings,
+    client_settings=chromadb.config.Settings(
+        is_persistent=False
+    )
+    )
     return vectorstore
 
 def search(vectorstore, query, k=3):
